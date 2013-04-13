1ff4ce813f7c6525d0f04a121508c99fd870ddd2
Eric Horacek <horacek.eric@gmail.com>
2013-04-13 16:17:25 -0600

    Adds a remind_at_s method to reminders

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,4 +1,12 @@
 class Reminder < ActiveRecord::Base
   attr_accessible :remind_at, :task_id
   belongs_to :task
+
+  def remind_at_s
+    if self.remind_at.nil?
+      return "No Remind At"
+    else
+      return self.remind_at.strftime("%l:%M %p on %b %e, %Y")
+    end
+  end
 end
